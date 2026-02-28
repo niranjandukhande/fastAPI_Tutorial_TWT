e1ef5b6db42bada7ac5789fee049596583ef4587
Niranjan Dukhande <progsolos@gmail.com>
2026-02-28 15:35:30 +0530

    feat: basic route definition and setup

--- app/app.py
+++ app/app.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+text_posts = {
+    1: {"title": "First Post", "content": "cool test post"},
+    2: {"title": "Second Post", "content": "cool test post 2"},
+}
+
+
+@app.get("/posts")
+def get_all_posts():
+    return text_posts
+
+
+@app.get(f"/posts/{id}")
+def get_post(id: int):
+    return text_posts.get(id)
